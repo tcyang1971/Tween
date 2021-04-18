ac6417c7282aa3aa50957ff8d5c4d8ed8574f8d1
tcyang <tcyang@gm.pu.edu.tw>
2021-04-19 07:55:29 +0800

    設定背景圖

--- app/src/main/java/tw/edu/pu/csim/tcyang/tween/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/tween/MainActivity.kt
@@ -1,11 +1,24 @@
 package tw.edu.pu.csim.tcyang.tween
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var txv: TextView
+    lateinit var cl: ConstraintLayout
+    lateinit var am : Animation  //動畫特效
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        txv = findViewById(R.id.txv)
+        cl = findViewById(R.id.cl)
+        cl.setBackgroundResource(R.drawable.salmon)
     }
 }
