358c52e1c59805fbaf7bba711c396b310937bf45
tcyang <tcyang@gm.pu.edu.tw>
2021-04-19 08:15:19 +0800

    觸控螢幕，讓圖形淡入

--- app/src/main/java/tw/edu/pu/csim/tcyang/tween/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/tween/MainActivity.kt
@@ -1,6 +1,8 @@
 package tw.edu.pu.csim.tcyang.tween
 
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -20,5 +22,18 @@ class MainActivity : AppCompatActivity() {
         txv = findViewById(R.id.txv)
         cl = findViewById(R.id.cl)
         cl.setBackgroundResource(R.drawable.salmon)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        //1.AlphaAnimation 起始透明度,最後透明度(Alpha)值
+        am = AlphaAnimation(0.0f, 1.0f)
+        //設定動畫開始到結束的執行時間
+        am.duration = 5000
+        //設定重複次數，-1為無限次數
+        am.repeatCount = 0
+
+        //將動畫特效設定到背景圖並開始執行動畫
+        cl.startAnimation(am)
+        return false
     }
 }
